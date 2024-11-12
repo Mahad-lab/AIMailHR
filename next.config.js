447fa05fd5146b7fb4ba3883a832dff4700b679d
Mahad <56235065+Mahad-lab@users.noreply.github.com>
2024-11-12 08:37:06 +0000

    Update packages and config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  env: {
+    GROQ_API_KEY: process.env.GROQ_API_KEY,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
